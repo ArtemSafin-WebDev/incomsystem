4bac7ccd752af691687ec62c20783f406ca07363
Artem Safin <artemsafin.webdev@gmail.com>
2026-04-04 19:44:41 +0300

    По первой секции

--- pages-data/home.js
+++ pages-data/home.js
@@ -1,5 +1,23 @@
 export default {
   "/index.html": {
     title: "Главная страница",
+    hero: {
+      title: "Инкомсистем",
+      description:
+        "один из востребованных системных интеграторов и производителей систем коммерческого учета в Российской Федерации",
+      caption: "Создан в 1991 году",
+      socials: [
+        {
+          href: "#",
+          label: "Telegram",
+          icon: "social/telegram",
+        },
+        {
+          href: "#",
+          label: "ВКонтакте",
+          icon: "social/vk",
+        },
+      ],
+    },
   },
 };
