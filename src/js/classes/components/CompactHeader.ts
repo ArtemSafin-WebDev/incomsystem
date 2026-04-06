b9c803b8457cd17693e9a8a9af69739594ef15cb
Artem Safin <artemsafin.webdev@gmail.com>
2026-04-06 03:38:40 +0300

    По адаптиву блока hero

--- src/js/classes/components/CompactHeader.ts
+++ src/js/classes/components/CompactHeader.ts
@@ -1,4 +1,5 @@
 import Component from "../Component";
+import { MOBILE_BREAKPOINT } from "../../constants/breakpoints";
 
 class CompactHeader extends Component {
   private static readonly COMPACT_SCROLL_OFFSET = 20;
@@ -32,6 +33,10 @@ class CompactHeader extends Component {
   }
 
   private shouldUseCompactMode(): boolean {
+    if (window.innerWidth <= MOBILE_BREAKPOINT) {
+      return false;
+    }
+
     return window.scrollY > CompactHeader.COMPACT_SCROLL_OFFSET;
   }
 }
